2dfae790c87f2f8650b1a907de4132dc75142323
Julien Melissas <julien.melissas@gmail.com>
2015-05-06 01:42:51 -0400

    Adding basic Leaflet map, Leaflet styles and scripts, etc... Also comments.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,1 +1,11 @@
+$(document).ready(function() {
+  var map = L.map('map', {
+    center: [35.59, -82.56], // lat, long
+    zoom: 13
+  });
 
+  L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      maxZoom: 18
+  }).addTo(map);
+});
