947e3c04429c29fd0e7cc8009826e5b8b03a23e0
Julien Melissas <julien.melissas@gmail.com>
2015-05-06 02:08:31 -0400

    Added button to get current location and put a pin on the map!

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,4 +1,25 @@
 $(document).ready(function() {
+  /*
+   * Application Functions
+   */
+  addCurrentLocationToMap = function() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        console.log('--- Your Position: ---');
+        console.log('Lat: ' + position.coords.latitude);
+        console.log('Long: ' + position.coords.longitude);
+        console.log('---------------------');
+
+        var marker = L.marker([position.coords.latitude, position.coords.longitude]).addTo(map);
+      });
+    } else {
+      console.log("Geolocation is not supported by this browser.");
+    }
+  }
+
+  /*
+   * Application Init
+   */
   var map = L.map('map', {
     center: [35.59, -82.56], // lat, long
     zoom: 13
@@ -9,5 +30,8 @@ $(document).ready(function() {
       maxZoom: 18
   }).addTo(map);
 
-  var marker = L.marker([35.59, -82.56]).addTo(map);
+  // This just adds the current location using the HTML5 API!
+  $('#get-location').click(function(event) {
+    addCurrentLocationToMap();
+  });
 });
