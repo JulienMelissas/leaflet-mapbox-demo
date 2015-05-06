25873952b1fb16d2190aa4479b7be9d55eedcc8e
Julien Melissas <julien.melissas@gmail.com>
2015-05-06 02:05:31 -0400

    Add marker to the map.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -8,4 +8,6 @@ $(document).ready(function() {
       attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
       maxZoom: 18
   }).addTo(map);
+
+  var marker = L.marker([35.59, -82.56]).addTo(map);
 });
